a722d5b7a006d785263209671e9c1a3ec5b2ba35
danRosof <daniel.rosofsky.ryan@gmail.com>
2022-12-11 20:56:54 -0700

    yes

--- music_sorter/settings.py
+++ music_sorter/settings.py
@@ -120,11 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = "../frontend/"
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "frontend/static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = "static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
