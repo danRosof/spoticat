d82c4ea0943bcbe62345ee4fc530569909b506d3
danRosof <daniel.rosofsky.ryan@gmail.com>
2022-12-12 00:19:40 -0700

    commit

--- music_sorter/settings.py
+++ music_sorter/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-zv5=1(lo@x04(jf9r#$($p(eu1&sb1@65z!q+oet&asq6_p!^7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*",
